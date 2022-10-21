a4892db072876c50a6539f977cfbd241dbd545d9
Servando Negrete <servandoo.12@hotmail.com>
2022-10-21 00:16:04 -0500

    footer modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -77,14 +77,8 @@ const Home = ({ leaderboard }: { leaderboard: ILeaderBoard[] }) => {
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        <a>
+          Powered by <b>GEITS</b>{' '}
         </a>
       </footer>
     </div>
