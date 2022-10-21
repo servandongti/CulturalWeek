5798e89589204dadbbaa56d4cac17d49f38deeac
Juanda1050 <danielalejandre1050@gmail.com>
2022-10-20 19:09:39 -0500

    Landing component finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,35 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { createClient } from '@supabase/supabase-js'
+import { Table, useCollator} from '@nextui-org/react';
 
-const Home: NextPage = () => {
+interface ILeaderBoard  {
+  id?: number;
+  score: number;
+  name: string;
+  phone: number;
+}
+
+export async function getStaticProps() {
+  const supabaseAdmin = createClient(
+    process.env.SUPABASE_URL || '',
+    process.env.SUPABASE_SERVICE_ROLE || ''
+  )
+  const { data } = await supabaseAdmin.from('leaderboard').select('*').order('id')
+  return {
+    props: {
+      leaderboard: data,
+    },
+  }
+}
+
+function cn(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+const Home = ({leaderboard}: {leaderboard: ILeaderBoard[]}) => {
+  console.log(leaderboard)
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -11,61 +38,42 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <h1 className={cn(
+        'text-transparent bg-clip-text font-extrabold',
+        'bg-gradient-to-r from-[#42d392] to-[#647eff]',
+        'text:2xl sm:text-4xl p-8'
+        )}>Leaderboard</h1>
+      <Table
+      bordered
+      shadow
+      color="secondary"
+      aria-label="Example pagination  table"
+      css={{
+        height: "auto",
+        minWidth: "100%",
+      }}
+      
+    >
+      <Table.Header>
+        <Table.Column allowsSorting>NAME</Table.Column>
+        <Table.Column>SCORE</Table.Column>
+        <Table.Column>PHONE</Table.Column>
+      </Table.Header>
+      <Table.Body>
+      {
+        leaderboard.map(({id,name, score, phone}) => (
+          <Table.Row key={id}>
+            <Table.Cell>{name}</Table.Cell>
+            <Table.Cell>{score}</Table.Cell>
+            <Table.Cell>{phone}</Table.Cell>
+          </Table.Row>
+        ))
+      }
+        
+      </Table.Body>
+      
+    </Table>
+     
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
